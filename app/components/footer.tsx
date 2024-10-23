dbacb05b6de73effe1d127abb54e84ea5702ce46
Ayanda <gatshaayanda@gmail.com>
2024-10-23 16:52:09 +0200

    v1.0.2

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -38,14 +38,7 @@ export default function Footer() {
   return (
     <small className="block lg:mt-24 mt-16 text-[#1C1C1C] dark:text-[#D4D4D4]">
       <time>© {YEAR}</time>{" "}
-      <a
-        className="no-underline"
-        href={socialLinks.twitter}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        {metaData.title}
-      </a>
+ 
       <style jsx>{`
         @media screen and (max-width: 480px) {
           article {
